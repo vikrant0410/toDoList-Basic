4b06708cd15c77a9dfd92a19f2a01c59c46a0e1f
vikrant0410 <vikrantpanchal11@gmail.com>
2021-06-12 17:51:10 +0530

    initial changes made

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+
+app.use(express.static("public"));
+
+app.set('view engine', "ejs")
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// mongoose
+mongoose.connect("mongodb+srv://tinku123:tinku123@cluster0.igsjh.mongodb.net/todolistDB", { useNewUrlParser: true, useUnifiedTopology: true });
+
+// schema
+const itemSchema = new mongoose.Schema({
+    item: {
+        type: String,
+        required: true
+    }
+})
+// model / collection
+const Items = mongoose.model("Item",itemSchema);
+
+
+// 
+app.get("/", (req, res) => {
+    // res.sendFile(__dirname + "");
+  Items.find({},(err,foundItems)=>{
+      if(err){
+          console.log(err)
+      }else{
+           res.render("list",{
+               myListItems : foundItems
+           })
+      }
+  })
+ 
+})
+
+app.post("/", (req, res) => {
+    var listItem = req.body.newItem;
+   var item = new Items({
+       item:listItem
+   })
+   item.save();
+res.redirect("/");
+});
+
+
+
+
+app.listen(3000, () => {
+    console.log("server started on port : 3000");
+})
